0503bc326f7fb771c9345774fe89db5a5d227425
Ivan <ivan@primaguna.com>
2011-12-11 04:30:47 +0700

    Test

--- protected/views/jenis/view.php
+++ protected/views/jenis/view.php
@@ -1,19 +1,18 @@
 <?php
 $this->breadcrumbs=array(
-	'Jenis Barangs'=>array('index'),
-	$model->id,
+	'Jenis Barang'=>array('index'),
+	$model->nama,
 );
 
 $this->menu=array(
-	array('label'=>'List JenisBarang', 'url'=>array('index')),
-	array('label'=>'Create JenisBarang', 'url'=>array('create')),
-	array('label'=>'Update JenisBarang', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete JenisBarang', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage JenisBarang', 'url'=>array('admin')),
+	array('label'=>'Daftar Jenis Barang', 'url'=>array('index')),
+	array('label'=>'Tambahkan Jenis Barang', 'url'=>array('create')),
+	array('label'=>'Update Detail Jenis Barang', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Hapus JenisBarang', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Anda yakin mau menghapus jen?')),
 );
 ?>
 
-<h1>View JenisBarang #<?php echo $model->id; ?></h1>
+<h1><?php echo $model->nama; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
